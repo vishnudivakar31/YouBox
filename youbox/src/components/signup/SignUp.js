4e86fd7170b988cc02fc9cb3a1e4a5227f94ebc8
Vishnu Divakar <vishnudivakar31@gmail.com>
2020-12-25 18:15:29 -0500

    signup form validation

--- youbox/src/components/signup/SignUp.js
+++ youbox/src/components/signup/SignUp.js
@@ -1,5 +1,5 @@
 import { Button } from "@material-ui/core";
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import Container from '@material-ui/core/Container'
 import Box from '@material-ui/core/Box'
 import TextField from '@material-ui/core/TextField'
@@ -22,26 +22,164 @@ class SignUp extends Component {
                 cond3: false,
                 cond4: false,
                 cond5: false,
-                cond6: false
+                cond6: false,
+                cond7: false
+            },
+            form_error: {
+                field1: {
+                    error: false,
+                    msg: ""
+                },
+                field2: {
+                    error: false,
+                    msg: ""
+                },
+                field3: {
+                    error: false,
+                    msg: ""
+                },
+                field4: {
+                    error: false,
+                    msg: ""
+                }
             }
         }
+        this.nameRef = createRef()
+        this.emailRef = createRef()
+        this.passwordRef = createRef()
+        this.confirmPasswordRef = createRef()
+        this.passwordValidator = this.passwordValidator.bind(this)
+        this.confirmPasswordHandler = this.confirmPasswordHandler.bind(this)
+        this.submit = this.submit.bind(this)
     }
+
+    submit() {
+        let field1 = {
+            error: false,
+            msg: ""
+        }
+        let field2 = {
+            error: false,
+            msg: ""
+        }
+        let field3 = {
+            error: false,
+            msg: ""
+        }
+        let field4 = {
+            error: false,
+            msg: ""
+        }
+        if(this.nameRef.current === undefined || this.nameRef.current.value.length === 0) {
+            field1 = {
+                error: true,
+                msg: 'required field'
+            }
+        }
+        if(this.emailRef.current === undefined || this.emailRef.current.value.length === 0) {
+            field2 = {
+                error: true,
+                msg: 'required field'
+            }
+        }
+        if(this.passwordRef.current === undefined || this.passwordRef.current.value.length === 0) {
+            field3 = {
+                error: true,
+                msg: 'required field'
+            }
+        }
+        if(this.confirmPasswordRef.current === undefined || this.confirmPasswordRef.current.value.length === 0) {
+            field4 = {
+                error: true,
+                msg: 'required field'
+            }
+        }
+        this.setState({
+            form_error:{
+                field1,
+                field2,
+                field3,
+                field4
+            }
+        })
+    }
+
+    confirmPasswordHandler(event) {
+        const password = this.passwordRef.current.value
+        const confirmPassword = event.target.value
+        let confirm = false
+        if(password === confirmPassword) {
+            confirm = true
+        }
+        this.setState({
+            form_validity: {
+                cond1: this.state.form_validity.cond1,
+                cond2: this.state.form_validity.cond2,
+                cond3: this.state.form_validity.cond3,
+                cond4: this.state.form_validity.cond4,
+                cond5: this.state.form_validity.cond5,
+                cond6: this.state.form_validity.cond6,
+                cond7: confirm
+            }
+        })
+    }
+
+    passwordValidator(event) {
+        let password = event.target.value
+        let cond1 = false, cond2 = true, cond3 = false, cond4 = false, cond5 = false, cond6 = false
+        const cond3Regex = /(?=.*[0-9])/
+        const cond4Regex = /(?=.*[!@#$!])/
+        const cond5Regex = /[A-Z]/
+        const cond6Regex = /[a-z]/
+        if(password.length >= 6) {
+            cond1 = true
+        }
+        if(password.length > 16) {
+            cond2 = false
+        }
+        if(password.match(cond3Regex)) {
+            cond3 = true
+        }
+        if(password.match(cond4Regex)) {
+            cond4 = true
+        }
+        if(password.match(cond5Regex)) {
+            cond5 = true
+        }
+        if(password.match(cond6Regex)) {
+            cond6 = true
+        }
+        this.setState({
+            form_validity: {
+                cond1,
+                cond2,
+                cond3,
+                cond4,
+                cond5,
+                cond6,
+                cond7: this.state.form_validity.cond7
+            }
+        })
+    }
+
     render() {
-        let icons = [
+        const icons = [
             this.state.form_validity.cond1 ? <CheckIcon /> : <CancelIcon />,
             this.state.form_validity.cond2 ? <CheckIcon /> : <CancelIcon />,
             this.state.form_validity.cond3 ? <CheckIcon /> : <CancelIcon />,
             this.state.form_validity.cond4 ? <CheckIcon /> : <CancelIcon />,
             this.state.form_validity.cond5 ? <CheckIcon /> : <CancelIcon />,
-            this.state.form_validity.cond6 ? <CheckIcon /> : <CancelIcon />
+            this.state.form_validity.cond6 ? <CheckIcon /> : <CancelIcon />,
+            this.state.form_validity.cond7 ? <CheckIcon /> : <CancelIcon />
         ]
-        let colors = [
+        const colors = [
             this.state.form_validity.cond1 ? 'green' : 'red',
             this.state.form_validity.cond2 ? 'green' : 'red',
             this.state.form_validity.cond3 ? 'green' : 'red',
             this.state.form_validity.cond4 ? 'green' : 'red',
             this.state.form_validity.cond5 ? 'green' : 'red',
-            this.state.form_validity.cond6 ? 'green' : 'red'
+            this.state.form_validity.cond6 ? 'green' : 'red',
+            this.state.form_validity.cond7 ? 'green' : 'red'
         ]
         return(
             <Container fixed className='signup'>
@@ -99,6 +237,13 @@ class SignUp extends Component {
                                     </ListItemIcon>
                                     <ListItemText primary="Should contain atleast one lowercase letter" />
                                 </ListItem>
+
+                                <ListItem style={{ color: colors[6]}}>
+                                    <ListItemIcon style={{ color: colors[6]}}>
+                                        {icons[6]}
+                                    </ListItemIcon>
+                                    <ListItemText primary="Both passwords should match" />
+                                </ListItem>
                             </List>
                         </Container>
                         <Container>
@@ -106,27 +251,66 @@ class SignUp extends Component {
                                 Create Account
                             </Box>
                             <Box>
-                                <TextField id="name_text" label="Name" variant="outlined" type='username' size='medium' fullWidth className='form_input' />
+                                <TextField 
+                                    id="name_text" 
+                                    label="Name" 
+                                    variant="outlined" 
+                                    type='username' 
+                                    size='medium' 
+                                    fullWidth 
+                                    className='form_input'
+                                    inputRef={this.nameRef}
+                                    error={this.state.form_error.field1.error}
+                                    helperText={this.state.form_error.field1.msg}
+                                />
                             </Box>
                             <Box>
-                                <TextField id="email_text" label="email" variant="outlined" type='username' fullWidth className='form_input' />
+                                <TextField 
+                                    id="email_text" 
+                                    label="email" 
+                                    variant="outlined" 
+                                    type='username' 
+                                    fullWidth 
+                                    className='form_input'
+                                    inputRef={this.emailRef}
+                                    error={this.state.form_error.field2.error}
+                                    helperText={this.state.form_error.field2.msg}
+                                />
                             </Box>
                             <Box>
-                                <TextField id="password_text" label="password" variant="outlined" type='password' fullWidth className='form_input' />
+                                <TextField 
+                                    id="password_text" 
+                                    label="password" 
+                                    variant="outlined" 
+                                    type='password' 
+                                    fullWidth 
+                                    className='form_input'
+                                    inputRef={this.passwordRef}
+                                    onChange={this.passwordValidator}
+                                    error={this.state.form_error.field3.error}
+                                    helperText={this.state.form_error.field3.msg}
+                                />
                             </Box>
                             <Box>
-                                <TextField id="confirm_password_text" label="confirm password" variant="outlined" type='password' fullWidth className='form_input' />
+                                <TextField 
+                                    id="confirm_password_text" 
+                                    label="confirm password" 
+                                    variant="outlined" 
+                                    type='password' 
+                                    fullWidth 
+                                    className='form_input' 
+                                    inputRef={this.confirmPasswordRef}
+                                    onChange={this.confirmPasswordHandler}
+                                    error={this.state.form_error.field4.error}
+                                    helperText={this.state.form_error.field4.msg}
+                                />
                             </Box>
                             <Box>
                                 <Button 
                                     variant='contained' 
                                     color='primary' 
-                                    className='form_input' 
-                                    disabled={!(this.state.form_validity.cond1 &&
-                                        this.state.form_validity.cond2 &&
-                                        this.state.form_validity.cond3 &&
-                                        this.state.form_validity.cond4 &&
-                                        this.state.form_validity.cond5)}
+                                    className='form_input'
+                                    onClick={this.submit}
                                 >
                                     Submit
                                 </Button>
