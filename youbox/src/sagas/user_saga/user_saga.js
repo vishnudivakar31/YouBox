64f757bc11b9433f86cd5b5c91056fae06e169a6
Vishnu Divakar <vishnudivakar31@gmail.com>
2020-12-26 12:55:31 -0500

    firebase saga integration

--- youbox/src/sagas/user_saga/user_saga.js
+++ youbox/src/sagas/user_saga/user_saga.js
@@ -1,8 +1,12 @@
 import { takeEvery, call, put } from 'redux-saga/effects'
 import { REGISTER_USER } from './action_type'
+import { reduxSagaFirebase } from '../../firebase/Firebase'
 
 function* registerUser(user) {
-    console.log('user-details', user)
+    const email = user.payload.email
+    const password = user.payload.password
+    console.log('email', email)
+    console.log('password', password)
 }
 
 export default function* userSaga() {
