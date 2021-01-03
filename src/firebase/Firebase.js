2248f2716efe53d11ac17d4d8dd2564c8e5e6c71
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-01-03 18:17:38 -0500

    config changes

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -2,13 +2,22 @@ import firebase from 'firebase'
 import '@firebase/firestore'
 import ReduxSagaFirebase from 'redux-saga-firebase'
 
+const {
+    REACT_APP_FIREBASE_API_KEY, 
+    REACT_APP_FIREBASE_AUTH_DOMAIN, 
+    REACT_APP_FIREBASE_PROJECT_ID, 
+    REACT_APP_FIREBASE_STORAGE_BUCKET, 
+    REACT_APP_FIREBASE_MESSAGING_ID, 
+    REACT_APP_FIREBASE_APP_ID
+} = process.env
+
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_ID,
+    appId: REACT_APP_FIREBASE_APP_ID
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
